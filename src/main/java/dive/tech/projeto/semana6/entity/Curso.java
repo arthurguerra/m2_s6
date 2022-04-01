6a9db08addef1bc89fee29d17770202bb85886d9
Arthur Guerra <arthur_gb1996@hotmail.com>
2022-04-01 05:42:20 -0300

    adiciona pacote entity

---
+++ src/main/java/dive/tech/projeto/semana6/entity/Curso.java
@@ -0,0 +1,57 @@
+package dive.tech.projeto.semana6.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+public class Curso implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String nome;
+
+    @OneToMany
+    private List<Disciplina> disciplinas;
+
+    public Curso() {
+    }
+
+    public Curso(String nome) {
+        this.nome = nome;
+    }
+
+    public Curso(String nome, List<Disciplina> disciplinas) {
+        this.nome = nome;
+        this.disciplinas = disciplinas;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Disciplina> getDisciplinas() {
+        return disciplinas;
+    }
+
+    public void setDisciplinas(List<Disciplina> disciplinas) {
+        this.disciplinas = disciplinas;
+    }
+}
---
+++ src/main/java/dive/tech/projeto/semana6/entity/Disciplina.java
@@ -0,0 +1,48 @@
+package dive.tech.projeto.semana6.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Disciplina implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String nome;
+
+    public Disciplina() {
+    }
+
+    public Disciplina(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Disciplina{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+}
